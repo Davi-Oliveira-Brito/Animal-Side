bd77151ce8a4e6d225bdf3a07031179605c17cee
lalais <lais.s.almeidas@gmail.com>
2022-09-22 14:23:15 -0300

    endpoint cadastrar

--- api/src/Controller/cadastroController.js
+++ api/src/Controller/cadastroController.js
@@ -1,4 +1,4 @@
-import { buscaRaca, buscaPorte, buscaPreferencia} from "../Repository/cadastroRepository.js"
+import { buscaRaca, buscaPorte, buscaPreferencia, cadastrarAnimal} from "../Repository/cadastroRepository.js"
 
 import { Router } from "express";
 
@@ -21,6 +21,42 @@ server.get('/admin/buscaPorte', async (req,resp)=>{
     const porte = await buscaPorte();
     console.log(porte)
     resp.status(200).send(porte)
+})
+
+server.post('/admin/adocao', async (req, resp) => {
+    try{
+        const animalAdocao = req.body;
+
+        if(!animalAdocao.nome)
+          throw new Error ('Nome do animal é obrigatório!')
+
+        if(!animalAdocao.idade)
+          throw new Error ('Idade do animal é obrigatório!')
+
+        if(!animalAdocao.sexo)
+          throw new Error ('O sexo do animal é obrigatório!')
+        
+        if(!animalAdocao.descricao)
+          throw new Error ('A descricao sobre o animal é obrigatória!')
+
+        if(!animalAdocao.porte)
+          throw new Error ('O porte do animal é obrigatório!')
+
+        if(!animalAdocao.raca)
+          throw new Error ('A raça do animal é obrigatória!')
+
+        
+        if(!animalAdocao.preferencia)
+          throw new Error ('A preferência de lar do animal é obrigatória!')
+
+        const animalInserido = await cadastrarAnimal(animalAdocao);
+        resp.send(animalInserido);
+
+    }  catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
 })
 
 export default server;
--- api/src/Repository/cadastroRepository.js
+++ api/src/Repository/cadastroRepository.js
@@ -33,3 +33,13 @@ export async function buscaPreferencia()
     const [resposta] = await con.query(comando, []);
     return resposta;   
 }
+
+export async function cadastrarAnimal(animal) {
+    const comando =
+        `INSERT INTO tb_animal_adocao(nm_animal,nr_idade,ds_sexo,ds_descricao,id_porte,id_raca,id_comentario_adocao,id_preferencia)
+              VALUES(?, ?, ?, ?, ?, ?, ?, ?)`    
+
+    const [ resposta ] = await con.query(comando[animal.adocao, animal.idade, animal.sexo, animal.descricao, animal.porte, animal.raca, animal.comentario, animal.preferencia]);
+    
+    return resposta;
+}
