97809781adb625e9b901e8adbbcf6e11e9b4d06b
Gustavo Alves <www.guga45@hotmail.com>
2022-09-28 15:24:08 -0300

    Adicionando Swagger

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,30 @@
+// Enviroment
 import 'dotenv/config';
 
+// Express
 import express from 'express';
 import cors from 'cors';
 
+// Swagger
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs'
 
+// Routes
 import adminController from './Controller/adminController.js'
 import cadastroController from './Controller/cadastroController.js'
 
+const swaggerOptions = JSON.parse(fs.readFileSync('./src/swagger.json'));
 
-
+// Configuração
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 
+
 server.use(adminController);
 server.use(cadastroController);
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`))
