486c5767aeacd4808dece1b5c6e6d665805bb8ca
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-09-29 22:15:55 -0300

    dml filtros da page feito

--- DB/dml.sql
+++ DB/dml.sql
@@ -2,7 +2,19 @@ use sistemaanimalside;
 
 -- isert inicial do adm
 insert into tb_admin(nm_admin, ds_email, ds_senha)
-			  values('Administrador','admin@admin','1234');
+			  values('Administrador','admin@admin','admin');
+              
+insert into tb_raca(ds_raca)
+				values('Vira-Late');
+                
+                
+select * from tb_animal_adocao; 
+                
+insert into tb_preferencia(ds_preferencia)
+				   values('Apartamento');
+                   
+insert into tb_porte(ds_porte)
+			  values('G');
 
 -- select da login-page
 select id_admin,
@@ -33,17 +45,46 @@ from	tb_preferencia;
 -- insert da tabela adocao
 insert into tb_animal_adocao(nm_animal,nr_idade,ds_sexo,ds_descricao,img_animal,id_usuario,id_porte,id_raca,id_comentario_adocao,id_preferencia)
 			  values('Apolo',12,'Macho','aaaa','',1,1,1,1,1);
+              
+              
+			
+-- select das dog adoção
+select * from tb_animal_adocao;
+
+-- select da barra de pesquisa
+select * from tb_animal_adocao
+where nm_animal like 'Cachorro%';
+
+-- filtros
+select * from tb_animal_adocao
+where id_raca = 1;
+
+select * from tb_animal_adocao
+where ds_sexo = 'Macho';
+
+select * from tb_animal_adocao
+where ds_sexo = 'Femea';
+
+select * from tb_animal_adocao
+where id_porte = 2;
+
+select * from tb_animal_adocao
+where id_porte = 3;
 
 
--- insert da raça
-insert into tb_raca(ds_raca)
-                values('Vira-Late');
+select * from tb_animal_adocao
+where nr_idade <=5;
 
--- insert da preferencia                
-insert into tb_preferencia(ds_preferencia)
-                   values('Apartamento');
-                   
--- insert da porte				   
-insert into tb_porte(ds_porte)
-              values('G');
+select * from tb_animal_adocao
+where nr_idade <=10;
+
+select * from tb_animal_adocao
+where nr_idade >10;
+
+
+
+
+
+
+
               
