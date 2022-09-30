0f4209113581b600772830acae885eed53e2adca
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-09-30 16:59:29 -0300

    componente cardFeed feito

---
+++ site/src/components/cardFeed/index.js
@@ -0,0 +1,31 @@
+import './index.scss'
+
+export default function CardFeed(props) {
+    return (
+        <div className="cards-mom">
+
+            <div className="carde">
+
+                <img className="card-image" src="/assets/images/dogBranco.png" alt="" />
+
+                <div className="left-card">
+                    <div className="text-card">
+                        <p className="titleCard">{props.nome}</p>
+                        <p className="little-letters">{props.visto}</p>
+                        <p className="little-desc">{props.desc} </p>
+                        <p className="little-letters">{props.idade}</p>
+                    </div>
+
+                    <div className="buttons-div">
+                        <div className="butoes">
+                            <button className="butao">Conhecer</button>
+                            <button className="butao">Adotar</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
--- site/src/pages/adote/feed/index.js
+++ site/src/pages/adote/feed/index.js
@@ -1,6 +1,7 @@
 import './index.scss'
 import NavBar from '../../../components/navBar/index.js'
 import SideBar from '../../../components/sideBar/index.js'
+import CardFeed from '../../../components/cardFeed/index.js'
 
 export default function(){
 
@@ -15,126 +16,9 @@ export default function(){
                 <div className="comp-side">
                     <SideBar/>
                 </div>
-                <div className="cards-mom">
-                    <div className="carde">
-        
-                            <img className="card-image"  src="/assets/images/dogBranco.png" alt="" />
-                        
-                        <div className="left-card">
-                            <div className="text-card">
-                                <p className="titleCard">Doguinho Daora</p>
-                                <p className="little-letters">Visto em VELHEIROS</p>
-                                <p className="little-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque audantium sed ut perspiciatis unde </p>
-                                <p className="little-letters">10 anos</p>
-                                </div>
-                            
-                            <div className="butoes">
-                                <button className="butao">Conhecer</button>
-                                <button className="butao">Adotar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="carde">
-        
-                            <img className="card-image"  src="/assets/images/dogBranco.png" alt="" />
-                        
-                        <div className="left-card">
-                            <div className="text-card">
-                                <p className="titleCard">Doguinho Daora</p>
-                                <p className="little-letters">Visto em VELHEIROS</p>
-                                <p className="little-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque audantium sed ut perspiciatis unde </p>
-                                <p className="little-letters">10 anos</p>
-                                </div>
-                            
-                            <div className="butoes">
-                                <button className="butao">Conhecer</button>
-                                <button className="butao">Adotar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="carde">
-        
-                            <img className="card-image"  src="/assets/images/dogBranco.png" alt="" />
-                        
-                        <div className="left-card">
-                            <div className="text-card">
-                                <p className="titleCard">Doguinho Daora</p>
-                                <p className="little-letters">Visto em VELHEIROS</p>
-                                <p className="little-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque audantium sed ut perspiciatis unde </p>
-                                <p className="little-letters">10 anos</p>
-                                </div>
-                            
-                            <div className="butoes">
-                                <button className="butao">Conhecer</button>
-                                <button className="butao">Adotar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="carde">
-        
-                            <img className="card-image"  src="/assets/images/dogBranco.png" alt="" />
-                        
-                        <div className="left-card">
-                            <div className="text-card">
-                                <p className="titleCard">Doguinho Daora</p>
-                                <p className="little-letters">Visto em VELHEIROS</p>
-                                <p className="little-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque audantium sed ut perspiciatis unde </p>
-                                <p className="little-letters">10 anos</p>
-                                </div>
-                            
-                            <div className="butoes">
-                                <button className="butao">Conhecer</button>
-                                <button className="butao">Adotar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="carde">
-        
-                            <img className="card-image"  src="/assets/images/dogBranco.png" alt="" />
-                        
-                        <div className="left-card">
-                            <div className="text-card">
-                                <p className="titleCard">Doguinho Daora</p>
-                                <p className="little-letters">Visto em VELHEIROS</p>
-                                <p className="little-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque audantium sed ut perspiciatis unde </p>
-                                <p className="little-letters">10 anos</p>
-                                </div>
-                            
-                            <div className="butoes">
-                                <button className="butao">Conhecer</button>
-                                <button className="butao">Adotar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="carde">
-        
-                            <img className="card-image"  src="/assets/images/dogBranco.png" alt="" />
-                        
-                        <div className="left-card">
-                            <div className="text-card">
-                                <p className="titleCard">Doguinho Daora</p>
-                                <p className="little-letters">Visto em VELHEIROS</p>
-                                <p className="little-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque audantium sed ut perspiciatis unde </p>
-                                <p className="little-letters">10 anos</p>
-                                </div>
-                            
-                            <div className="butoes">
-                                <button className="butao">Conhecer</button>
-                                <button className="butao">Adotar</button>
-                            </div>
-                        </div>
-                    </div>  
-
-                    
-
-
-
-                </div>
+                
+                <CardFeed nome="Cachorrinho Daora" visto="VELHEIROS"desc="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+                doloremque audantium sed ut perspiciatis unde " idade="11 Anos" />
 
 
             </div>
