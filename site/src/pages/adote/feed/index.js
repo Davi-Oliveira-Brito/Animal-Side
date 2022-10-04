b77a415a8d751a253461a8204d20917ee08a6a2f
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-10-04 11:23:17 -0300

    page feed comum

--- site/src/pages/adote/feed/index.js
+++ site/src/pages/adote/feed/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react';
+import React, { useEffect } from 'react';
 import './index.scss'
 import NavBar from '../../../components/navBar/index.js'
 import SideBar from '../../../components/sideBar/index.js'
@@ -8,7 +8,7 @@ import { adocaoCard } from '../../../api/feed.js';
 
 import { useState } from 'react';
 
-export default function(){
+export default function () {
     const [animais, setAnimais] = useState([]);
 
     async function carregarAnimais() {
@@ -18,36 +18,38 @@ export default function(){
 
     console.log(animais);
 
-    useEffect(()=>{
+    useEffect(() => {
         carregarAnimais();
-    },[]);
-    return(
+    }, []);
+    return (
         <main className="feedmain">
-            <NavBar/>
+            <NavBar />
             <div className="top">
-               <img className="dog-procure" src="/assets/images/orangeDog.png" alt="" /> 
-               <input className="pesquisa" type="text" placeholder="Procure seu Bichinho..."  />
+                <img className="dog-procure" src="/assets/images/orangeDog.png" alt="" />
+                <input className="pesquisa" type="text" placeholder="Procure seu Bichinho..." />
             </div>
             <div className="bottom">
                 <div className="comp-side">
-                    <SideBar/>
+                    <SideBar />
                 </div>
-                {animais.map(item => {
-                    return(
-                        <CardFeed 
-                        nome={item.nome} 
-                        idade={item.idade}
-                        descricao={item.descricao}
-                        imagem={item.idade} 
-                        porte={item.porte}
-                        raca={item.raca}
-                        preferencia={item.preferencia}
-                        sexo={item.sexo}
-                        tipo={item.tipo}
-                        />
-                    );
-                })}
-            
+                <div className='card-s'>
+                    {animais.map(item => {
+                        return (
+                            <CardFeed
+                                nome={item.nome}
+                                idade={item.idade}
+                                descricao={item.descricao}
+                                imagem={item.idade}
+                                porte={item.porte}
+                                raca={item.raca}
+                                preferencia={item.preferencia}
+                                sexo={item.sexo}
+                                tipo={item.tipo}
+                            />
+                        );
+                    })}
+                </div>
+
             </div>
         </main>
     )
