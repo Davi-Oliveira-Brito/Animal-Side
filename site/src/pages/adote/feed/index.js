3eff6de734a70a50582b095fffd19dd55a2ecba1
Gustavo Alves Vieira <www.guga45@hotmail.com>
2022-10-04 22:20:10 -0300

    teste

--- site/src/pages/adote/feed/index.js
+++ site/src/pages/adote/feed/index.js
@@ -28,7 +28,7 @@ export default function () {
                 <img className="dog-procure" src="/assets/images/orangeDog.png" alt="" />
                 <input className="pesquisa" type="text" placeholder="Procure seu Bichinho..." />
             </div>
-            <div></div>
+            <div> testeeeeeeeee </div>
             <div className="bottom">
                 <div className="comp-side">
                     <SideBar />
