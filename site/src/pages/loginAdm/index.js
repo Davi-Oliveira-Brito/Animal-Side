a429d674a61eb8f05d72733adb698d45ec14b248
lalais <lais.s.almeidas@gmail.com>
2022-10-07 02:22:32 -0300

    parte da login adm

--- site/src/pages/loginAdm/index.js
+++ site/src/pages/loginAdm/index.js
@@ -53,9 +53,10 @@ export default function LoginAdmin() {
 
                 <button className="admin-login-button" onClick={logar}>Entrar</button>
 
-                <p className="admin-crie-sua-conta">Não tem uma conta? Cadastre-se</p>
+                <p className="admin-crie-sua-conta">Não tem uma conta? <a>Cadastre-se</a></p>
 
                 <div className="admin-left">
+                    <button className="botao-direita">Pagina Inicial</button>
                     <div className="imagem"><img src="" alt="" /></div>
                 </div>
             </div>
