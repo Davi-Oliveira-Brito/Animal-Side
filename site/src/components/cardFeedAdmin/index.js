a52086c4c281725d3d145cb29f37add7e43d38ca
murilo <murilobalves04@gmail.com>
2022-10-08 22:49:52 -0300

    Ajuste Feed Admin

--- site/src/components/cardFeedAdmin/index.js
+++ site/src/components/cardFeedAdmin/index.js
@@ -4,18 +4,18 @@ export default function CardFeedAdmin(props) {
     return (
         <main className='Card'>
             <div className='image-left'>
-                <img src="/assets/images/dogBranco.png" alt="" />
+                <img className='imgdog' src="/assets/images/dogBranco.png" alt="" />
             </div>
             <div className='Card-right'>
                 <div className='text'>
                     <h1>{props.nome}</h1>
-                    <p>Raça: {props.raca}</p>
-                    <p>Porte: {props.porte}</p>
-                    <p>Idade: {props.idade}</p>
-                    <p>Pref: {props.pref}</p>
+                    <div>Raça: {props.raca}</div>
+                    <div>Porte: {props.porte}</div>
+                    <div>Idade: {props.idade}</div>
+                    <div>Pref: {props.pref}</div>
                 </div>
                 <div className='botao-img'>
-                    <div className='botao'>Situação</div>
+                    <button className='botao'>Situação</button>
                     <div className='img-interative'>
                         <img className='img' src="/assets/images/Edit.png" alt="" />
                         <img className='img' src="/assets/images/Close.png" alt="" />
