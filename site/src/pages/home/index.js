93d6067f12d4732c00deb1ce53be8974207a83cb
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-10-08 21:21:09 -0300

    faixa 2 lading page feita

--- site/src/pages/home/index.js
+++ site/src/pages/home/index.js
@@ -1,5 +1,6 @@
 import './index.scss'
 import NavBar from '../../components/navBar/index.js'
+import CardAdote from '../../components/LadingPage/CardAdote/index.js'
 
 export default function LadingPage() {
     return (
@@ -21,8 +22,44 @@ export default function LadingPage() {
 
                 </div>
                 <div className="img-hero">
-                        <img src="/assets/images/dogHero.png" alt="" />
+                    <img src="/assets/images/dogHero.png" alt="" />
+                </div>
+            </section>
+
+            <section className="faixa2-lading">
+                <div className="f2-sub">
+                    <div className="f2-top">
+                        <h1 className="title">escolha seu amigo perfeito</h1>
+                        <p className="text">Procure diretamente no filtro que você tem interesse em adotar.</p>
                     </div>
+                    <div className="f2-cards">
+                        <CardAdote 
+                        classe="azul"
+                        nome="CACHORROS"
+                        desc="Desde sempre seu melhor amigo"
+                        image="/assets/images/cachorrinho.png"
+                        />
+
+                        <CardAdote 
+                        classe="laranja"
+                        nome="GATOS"
+                        desc="Cat person ? Sem problemas temos esses felinos fofos também."
+                        image="/assets/images/gatinho.png"
+                        />
+
+                        <CardAdote 
+                        classe="verde"
+                        nome="PASSAROS"
+                        desc="Quer um animalzinho voador? Sem problema"
+                        image="/assets/images/papagaio.png"
+                        />
+
+
+                    </div>
+                    <div className="f2-button">
+                        <button>Mais</button>
+                    </div>
+                </div>
             </section>
         </main>
     );
