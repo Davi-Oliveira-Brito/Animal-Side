772da576ee4d54e2d9725ee197fbe990b48a9c7e
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-10-09 22:30:19 -0300

    faixas 3 e 4 lading page feitas

--- site/src/pages/home/index.js
+++ site/src/pages/home/index.js
@@ -33,25 +33,25 @@ export default function LadingPage() {
                         <p className="text">Procure diretamente no filtro que você tem interesse em adotar.</p>
                     </div>
                     <div className="f2-cards">
-                        <CardAdote 
-                        classe="azul"
-                        nome="CACHORROS"
-                        desc="Desde sempre seu melhor amigo"
-                        image="/assets/images/cachorrinho.png"
+                        <CardAdote
+                            classe="azul"
+                            nome="CACHORROS"
+                            desc="Desde sempre seu melhor amigo"
+                            image="/assets/images/cachorrinho.png"
                         />
 
-                        <CardAdote 
-                        classe="laranja"
-                        nome="GATOS"
-                        desc="Cat person ? Sem problemas temos esses felinos fofos também."
-                        image="/assets/images/gatinho.png"
+                        <CardAdote
+                            classe="laranja"
+                            nome="GATOS"
+                            desc="Cat person ? Sem problemas temos esses felinos fofos também."
+                            image="/assets/images/gatinho.png"
                         />
 
-                        <CardAdote 
-                        classe="verde"
-                        nome="PASSAROS"
-                        desc="Quer um animalzinho voador? Sem problema"
-                        image="/assets/images/papagaio.png"
+                        <CardAdote
+                            classe="verde"
+                            nome="PASSAROS"
+                            desc="Quer um animalzinho voador? Sem problema"
+                            image="/assets/images/papagaio.png"
                         />
 
 
@@ -59,6 +59,40 @@ export default function LadingPage() {
                     <div className="f2-button">
                         <button>Mais</button>
                     </div>
+                </div>
+            </section>
+
+            <section className="faixa3-lading">
+                
+            <img className="f3-dog" src="/assets/images/cachorrao.png" alt="" />
+
+                <div className="f3-sub">
+                    <div className="f3-item">
+                        <img src="/assets/images/maisAmor.png" alt="" />
+                        <p className="f3-title">Mais Amor</p>
+                        <p className="f3-desc1">Eu sonho com um mundo onde você e
+                            eu viveríamos por mil anos para nos amarmos</p>
+                    </div>
+
+                    <div className="f3-item">
+                        <img src="/assets/images/ajudaAnimais.png" alt="" />
+                        <p className="f3-title">Ajuda Aos Animais</p>
+                        <p className="f3-desc2">Ajudar os bichinhos não custa muito, faça o que puder. 
+                        Fazendo o básico, você pode ajudar um bichinho</p>
+                    </div>
+
+                    <div className="f3-item">
+                        <img src="/assets/images/umaNovaCasa.png" alt="" />
+                        <p className="f3-title">Uma Nova Casa</p>
+                        <p className="f3-desc3">Não faça parte do comércio clandestino de animais, ADOTE!</p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="f4-lading">
+                <div className="f4-sub">
+                    <p className="f4-title">AJUDE NOSSA CAUSA DOANDO </p>
+                    <p className="f4-desc">A <label>ANIMAL SIDE</label> ela se mantem através das doações, sejam as mesmas destinadas aos gastos específicos ou o valor que você conseguir dentro de seu orçamento.</p>
                 </div>
             </section>
         </main>
