84f076cb3e8890b4c87d5cc72466ce746b45cf6f
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2022-11-12 10:22:28 -0300

    page comentarios feita e implementada, tudo de bão

--- site/src/components/comentariosComp/index.js
+++ site/src/components/comentariosComp/index.js
@@ -4,11 +4,11 @@ export default function ComentarioComp(props) {
     return (
         <main className='comentario'>
             <div className='bola'>
-                <p>L</p>
+                <p>{props.primeiraLetra}</p>
             </div>
 
             <div className='text-comentario'>
-                <p className='coment-name'>Nome: {props.nome}</p>
+                <p className='coment-name'><label>Nome:</label> {props.nome}</p>
                 <p className='coment-desc'>{props.coment}</p>
             </div>
         </main>
--- site/src/pages/usuario/comentario/index.js
+++ site/src/pages/usuario/comentario/index.js
@@ -5,55 +5,54 @@ import SidebarUser from '../../../components/sidebarUsuario/index.js';
 import DadoUser from '../../../components/dados/index.js';
 import ComentarioComp from '../../../components/comentariosComp/index.js';
 
+// api
+
+import { mostrarComentariosFront } from '../../../api/usuario/usuarioAPI';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function UserComent() {
+
+    const [comentarios, setComentarios] = useState([]);
+
+    async function carregarComents() {
+        try {
+            const r = await mostrarComentariosFront();
+            setComentarios(r.result);
+            console.log(r)
+        } catch (error) {
+            toast.dark(error.response.data.error)
+        }
+    }
+
+    useEffect(() => {
+        carregarComents();
+    }, []);
+
 
     return (
         <main className="coment-page">
             <NavBarAdmin />
-            
-            <div className='coment-left'>
-                <SidebarUser/>
-            </div>
-            
-
+            <SidebarUser />
             <div className="coment-right">
                 <div className='dado'>
                     <DadoUser
-                    nome="Davi Oliveira Brito"
-                    regiao="São Paulo, SP"/>
+                        nome="Davi Oliveira Brito"
+                        regiao="São Paulo, SP" />
                 </div>
 
-                <div className='comentarios'>
-                    
-                    <ComentarioComp
-                    nome="Davi Oliveira Brito"
-                    coment="Eu vi um cachorro bem parecido com o da foto, perto do estacionamento do parque
-                    no dia 28/08 (domingo), por volta das 12hrs."
-                    />
-                    <ComentarioComp
-                    nome="Davi Oliveira Brito"
-                    coment="Eu vi um cachorro bem parecido com o da foto, perto do estacionamento do parque
-                    no dia 28/08 (domingo), por volta das 12hrs."
-                    />
-                    <ComentarioComp
-                    nome="Davi Oliveira Brito"
-                    coment="Eu vi um cachorro bem parecido com o da foto, perto do estacionamento do parque
-                    no dia 28/08 (domingo), por volta das 12hrs."
-                    />
-                    <ComentarioComp
-                    nome="Davi Oliveira Brito"
-                    coment="Eu vi um cachorro bem parecido com o da foto, perto do estacionamento do parque
-                    no dia 28/08 (domingo), por volta das 12hrs."
-                    />
-                    <ComentarioComp
-                    nome="Davi Oliveira Brito"
-                    coment="Eu vi um cachorro bem parecido com o da foto, perto do estacionamento do parque
-                    no dia 28/08 (domingo), por volta das 12hrs."
-                    />
-                    
+                <div className='comp'>
+                    {comentarios.map(item => {
+                        return (
+                            <ComentarioComp
+                                id={item.id_comentario}
+                                primeiraLetra={item.nm_usuario[0]}
+                                nome={item.nm_usuario}
+                                coment={item.ds_comentario}
+                            />
+                        )
+                    })}
                 </div>
-                
 
             </div>
         </main>
